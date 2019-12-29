5817c180f77664ef57b9bccedc22d339589e8023
Mrityunjay Singh <mrityunjay0510@gmail.com>
2019-12-29 13:29:03 +0530

    push code into master

---
+++ SeleniumNUnit/Base.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumNUnit
+{
+
+    //Base class
+    public class Base
+    {
+
+        public IWebDriver Driver { get; set; }
+
+    }
+}
---
+++ SeleniumNUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumNUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumNUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed93f4b3-b37b-48a2-b09d-1b6f1a1c0212")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
